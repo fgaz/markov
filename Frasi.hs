1ab62dec65c83de7e48654743024a33f8959d249
Francesco Gazzetta <fgaz@users.noreply.github.com>
2015-06-09 18:27:34 +0200

    generalization!

--- Frasi.hs
+++ Frasi.hs
@@ -1,39 +1,8 @@
-import System.IO
-import qualified Data.Map as M
-import System.Random
-import System.Environment
-import System.Exit
-
-type Probs a = M.Map a Int
-
-genFrase :: String -> Int -> StdGen -> String
-genFrase str len randGen = unwords $ reverse $ fst $ foldl addNextWord ([firstWord], randGen') [1..len]
-  where probs = genProb $ words str
-        (firstWord, randGen') = randomElement (M.keys probs) randGen
-        addNextWord :: ([String], StdGen) -> a -> ([String], StdGen)
-        addNextWord ((x:xs), randGen) _ = (next:x:xs, randGen')
-          where (next, randGen') = nextWord probs x randGen
-
-nextWord :: (Eq a, Ord a) => M.Map a (Probs a) -> a -> StdGen -> (a, StdGen)
-nextWord probs prevWord randGen = case mapProbs' of Just a  -> weightedRandom (M.toList a) randGen
-                                                    Nothing -> randomElement (M.keys probs) randGen --fallback to random word
-  where mapProbs' = M.lookup prevWord probs
-
-randomElement :: [a] -> StdGen -> (a, StdGen)
-randomElement list randGen = (list !! randomIndex , randGen')
-  where (randomIndex, randGen') = randomR (0,length list - 1) randGen
-
-weightedRandom :: [(a,Int)] -> StdGen -> (a, StdGen)
-weightedRandom list randGen = randomElement weightedList randGen
-  where weightedList = concat $ map (\(x,prob) -> replicate prob x) list
-
-
-genProb = addProb M.empty
-
-addProb :: (Eq a, Ord a) => M.Map a (Probs a) -> [a] -> M.Map a (Probs a)
-addProb p [] = p
-addProb p (_:[]) = p
-addProb p (x:y:xn) = addProb (M.insertWith (\_ a -> M.insertWith (+) y 1 a) x (M.singleton y 1) p) (y:xn)
+import System.IO (openFile, IOMode (ReadMode), hGetContents)
+import System.Random (getStdGen)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import Markov (genChain)
 
 
 main = do
@@ -46,4 +15,4 @@ main = do
     contents <- hGetContents handle
     randGen <- getStdGen
     let len = read $ head args
-    putStrLn $ genFrase contents len randGen
+    putStrLn $ unwords $ genChain (words contents) len randGen
---
+++ Markov.hs
@@ -0,0 +1,40 @@
+module Markov where
+
+import System.IO
+import qualified Data.Map as M
+import System.Random
+import System.Environment
+import System.Exit
+
+--type Probs a = M.Map a Int
+
+--type ChainData = (Eq a, Ord a) => M.Map a (Probs a)
+
+genChain :: (Ord a, Integral i) => [a] -> i -> StdGen -> [a]
+genChain list len randGen = reverse $ fst $ foldl addNextElem ([firstElem], randGen') [1..len]
+  where probs = genProb list
+        (firstElem, randGen') = randomElement (M.keys probs) randGen
+        --addNextElem :: (Eq a, Ord a) => ([a], StdGen) -> a -> ([a], StdGen)
+        addNextElem ((x:xs), randGen) _ = (next:x:xs, randGen')
+          where (next, randGen') = nextElem probs x randGen
+
+nextElem :: (Eq a, Ord a) => M.Map a (M.Map a Int) -> a -> StdGen -> (a, StdGen)
+nextElem probs prevWord randGen = case mapProbs' of Just a  -> weightedRandom (M.toList a) randGen
+                                                    Nothing -> randomElement (M.keys probs) randGen --fallback to random element
+  where mapProbs' = M.lookup prevWord probs
+
+randomElement :: [a] -> StdGen -> (a, StdGen)
+randomElement list randGen = (list !! randomIndex , randGen')
+  where (randomIndex, randGen') = randomR (0,length list - 1) randGen
+
+weightedRandom :: Integral i => [(a,i)] -> StdGen -> (a, StdGen)
+weightedRandom list randGen = randomElement weightedList randGen
+  where weightedList = concat $ map (\(x,prob) -> replicate (fromIntegral prob) x) list
+
+genProb :: (Eq a, Ord a, Integral i) => [a] -> M.Map a (M.Map a i)
+genProb = addProb M.empty
+
+addProb :: (Eq a, Ord a, Integral i) => M.Map a (M.Map a i) -> [a] -> M.Map a (M.Map a i)
+addProb p [] = p
+addProb p (_:[]) = p
+addProb p (x:y:xn) = addProb (M.insertWith (\_ a -> M.insertWith (+) y 1 a) x (M.singleton y 1) p) (y:xn)
